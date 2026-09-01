7f473a377817f22cfb55d0093fbf4f431c26b350
Celso <celsoromao9@gmail.com>
2026-09-01 20:38:24 -0300

    Atualização

---
+++ aulas/OperadoresLogicos.py
@@ -0,0 +1,19 @@
+#OPERADORES
+""""""
+ATRIBUIÇÃO
+
+= -> variavel = 10
+! = NÃO, NOT , CONTRARIOS...
+SIM -> !SIM = NÃO
+
+idade = 18
+
+COMPARAÇÃO
+esperar uma resposta de True ou False
+! = se for diferente retorna True, se for igual
+
+"""""""
+
+
+
+
