6612eaa2acc68ef4c3cea473dcfd89a785bb96c4
Celso <celsoromao9@gmail.com>
2026-09-01 21:25:30 -0300

    Atualização

--- aulas/OperadoresAritmeticos.py
+++ aulas/OperadoresAritmeticos.py
@@ -1,27 +1,21 @@
-#OPERADORES MATEMATICOS
+#OPERADORES MATEMÁTICOS
 numero1 = 10
 numero2 = 20
 
 #SOMA
 soma = numero1 + numero2
-
 #SUBTRAÇÃO
 subtracao = numero1 - numero2
-
 #MULTIPLICAÇÃO
 multiplicacao = numero1 * numero2
-
 #DIVISÃO
 divisao = numero1 / numero2
 resto = numero1 % numero2
 
 print("Soma dos números: ", soma)
-print("Subtracao dos números: ", subtracao)
-print("Multiplicacao dos números: ", multiplicacao)
-print("Divisao dos números: ", divisao)
+print("Subtração dos números: ", subtracao)
+print("Multiplicação dos números: ", multiplicacao)
+print("Divisão dos números: ", divisao)
 print("Resto dos números: ", resto)
-
-
-ll
 
 
