eaee52b2c9ca66a39508eb0e67d1259178cae88e
Celso <celsoromao9@gmail.com>
2026-09-08 21:09:05 -0300

    Resolução da Atividade6

---
+++ atividades/Atividade6/questao5.py
@@ -0,0 +1,8 @@
+# Questão 5: Tabuada Simples
+
+numero = int(input("Digite um número inteiro: "))
+contador = 1
+
+while contador <= 10:
+    print(f"{numero} x {contador} = {numero * contador}")
+    contador += 1
